43d55ac3da78d57c86772ef77ad208a9b95cb3be
wangpengpeng <pengpeng.wang@ishansong.com>
2019-09-02 13:06:48 +0800

    code

---
+++ precache-manifest.0e4153527c11acdb6916aa3d909dd60a.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98a66c58c8dff2e46b9e67afcfe4720b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e9e530d58a720d5295cd037f595d1fee",
+    "url": "./order.html"
+  },
+  {
+    "revision": "502360e79309cc78bae5ee49b2931b98",
+    "url": "./query.html"
+  },
+  {
+    "revision": "40fb2772d27b0a566267",
+    "url": "./static/css/0.764ccc25.chunk.css"
+  },
+  {
+    "revision": "929e046b76c17867baec",
+    "url": "./static/css/10.b40141b4.chunk.css"
+  },
+  {
+    "revision": "8000059b60dd6395e9b7",
+    "url": "./static/css/index.0503c8bf.chunk.css"
+  },
+  {
+    "revision": "8fda4da80953787aa240",
+    "url": "./static/css/order.37f51409.chunk.css"
+  },
+  {
+    "revision": "26aab5535cb3a5673bfb",
+    "url": "./static/css/query.5981a2ae.chunk.css"
+  },
+  {
+    "revision": "7dfe61f11f7a2fee4601",
+    "url": "./static/css/ticket.0c1ace16.chunk.css"
+  },
+  {
+    "revision": "40fb2772d27b0a566267",
+    "url": "./static/js/0.f7707cac.chunk.js"
+  },
+  {
+    "revision": "f4eac07768fbdfb0fc0a",
+    "url": "./static/js/1.6b772e02.chunk.js"
+  },
+  {
+    "revision": "929e046b76c17867baec",
+    "url": "./static/js/10.e5ae23bd.chunk.js"
+  },
+  {
+    "revision": "8000059b60dd6395e9b7",
+    "url": "./static/js/index.ae87da6d.chunk.js"
+  },
+  {
+    "revision": "8fda4da80953787aa240",
+    "url": "./static/js/order.46a2de2b.chunk.js"
+  },
+  {
+    "revision": "26aab5535cb3a5673bfb",
+    "url": "./static/js/query.5b040343.chunk.js"
+  },
+  {
+    "revision": "a2c89b0185e55918ad3e",
+    "url": "./static/js/runtime~index.9e337181.js"
+  },
+  {
+    "revision": "01001f24db3e86834b4a",
+    "url": "./static/js/runtime~order.a3459e5c.js"
+  },
+  {
+    "revision": "357982672e2a1479ecf3",
+    "url": "./static/js/runtime~query.ce81381b.js"
+  },
+  {
+    "revision": "9a2b5aefe68d4cf44c68",
+    "url": "./static/js/runtime~ticket.f9827908.js"
+  },
+  {
+    "revision": "7dfe61f11f7a2fee4601",
+    "url": "./static/js/ticket.f9b5ec77.chunk.js"
+  },
+  {
+    "revision": "b672b5fbfa02c97e5511f5b00155bb8c",
+    "url": "./static/media/switch.b672b5fb.svg"
+  },
+  {
+    "revision": "f019aaffe1dd6e4a61a3e4cd1a91c849",
+    "url": "./ticket.html"
+  }
+]);
